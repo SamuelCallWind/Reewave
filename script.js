05c66304746af663ed371b93dd46afdd8e1a52de
SamuelCallWind <Samcallwind@gmail.com>
2023-06-21 21:32:57 +0200

    Adding the bigger screen for days + JS to display it by clicking on days

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     const currentDate = new Date();
     let daysInMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate();
-
     let dayBoxes = document.querySelectorAll('.boxDay');
+    let calendar = document.querySelector(".calendar");
+
+    function dayBoxGrow() {
+        let createBox = document.createElement('div');
+        let createClose = document.createElement('div');
+        createClose.classList = "closeButton";
+        createClose.innerHTML = '&times;'
+        createBox.classList = "dayBoxGrow";
+        createBox.style.display = "block";
+        createBox.appendChild(createClose);
+        calendar.appendChild(createBox);
+    }
 
     for (i = 0; i < daysInMonth; i++) {
         dayBoxes[i].innerHTML = i + 1;
+        dayBoxes[i].addEventListener("click", dayBoxGrow)
     }
 
+    
+
+
+
 }); 
+
+    
