08c4e710664c9fca9af57b234709f2bb3dee0b63
SamuelCallWind <Samcallwind@gmail.com>
2023-06-23 21:04:51 +0200

    Many changes in the script to handle big box. Keeping the data instead of deleting it and creating it all the time

--- script.js
+++ script.js
@@ -6,43 +6,63 @@ window.addEventListener('DOMContentLoaded', (event) => {
     let currentMonth = new Date().toLocaleString('default', { month: 'long' });
     let newBox = document.createElement('div');
     let newClose = document.createElement('div');
+    let boxData = new Array(daysInMonth).fill(null);
 
     function dayBoxGrow(e) {
-        newBox.classList.add('dayBoxGrow');
-        newBox.style.display = 'block';
-        newBox.appendChild(createCloseButton());
+        let boxIndex = parseInt(e.target.innerHTML, 10) - 1;
+        if (boxIndex[boxData]) {
+            newBox.innerHTML = boxData[boxIndex];
+        } else {
+            newBox.innerHTML = ''; 
+
+            newBox.classList.add('dayBoxGrow');
+            newBox.style.display = 'block';
+            newBox.appendChild(createCloseButton());
+            createDayLine();
+
+            let boxTitle = document.createElement('h1');
+            boxTitle.innerHTML = `${currentMonth}  ${e.target.innerHTML}`;
+            newBox.appendChild(boxTitle);
+            
+            // Adding the two buttons to switch from day and night
+            newBox.appendChild(document.createElement('div')).classList.add('switchButtonOne');
+            newBox.lastChild.innerHTML = 'Day';
+            newBox.appendChild(document.createElement('div')).classList.add('switchButtonTwo');
+            newBox.lastChild.innerHTML = 'Night';
+
+            let saveButton = document.createElement('div');
+            saveButton.classList.add('saveChanges');
+            saveButton.innerHTML = 'Save';
+            saveButton.addEventListener('click', () => {
+                boxData[boxIndex] = newBox.innerHTML;
+            });
+            newBox.appendChild(saveButton);
+        }
         calendar.appendChild(newBox);
-        let boxTitle = document.createElement('h1');
-        boxTitle.innerHTML = `${currentMonth}  ${e.target.innerHTML}`;
-        newBox.appendChild(boxTitle);
-        createDayLine(newBox);
-        // Adding the two buttons to switch from day and night 
-        newBox.appendChild(document.createElement('div')).classList.add('switchButtonOne');
-        newBox.appendChild(document.createElement('div')).classList.add('switchButtonTwo');
-        
+    
     }
     function createCloseButton() {
         newClose.addEventListener('click', () => {
             newBox.style.display = 'none';
-            newBox.innerHTML = '';
+            // newBox.innerHTML = '';
         });   
         newClose.classList = 'closeButton';
         newClose.innerHTML = '&times;';
         return newClose;
     }
     
-    function createDayLine(box) {
+    function createDayLine() {
         let dayLine = document.createElement('div')
         dayLine.classList = 'dayLine';
         newBox.appendChild(dayLine);
 
-        for (i = 0; i < 10; i++) {
+        for (i = 0; i < 13; i++) {
             let verticalLine = document.createElement('div');
             let hourVerticalLine = document.createElement('p');
-            hourVerticalLine.classList = 'hourVerticalLine';
+            hourVerticalLine.classList = `hourVerticalLine hour${i+8}`;
             verticalLine.classList = 'verticalLine';
-            verticalLine.style.left = `${i *8.87 + 10}%`;
-            hourVerticalLine.style.left = `${i *8.8 + 8.60}%`;
+            verticalLine.style.left = `${i *7.28 + 5}%`;
+            hourVerticalLine.style.left = `${i *7.20 + 5}%`;
             hourVerticalLine.innerHTML = `${i + 8}:00`
             newBox.appendChild(hourVerticalLine);
             newBox.appendChild(verticalLine);
@@ -52,7 +72,6 @@ window.addEventListener('DOMContentLoaded', (event) => {
     for (i = 0; i < daysInMonth; i++) {
         dayBoxes[i].innerHTML = i + 1;
         dayBoxes[i].addEventListener('click', (e) => dayBoxGrow(e));
-        
         
     }
 
