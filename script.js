ecb6b4d2461ab79ae27963520fb58b64872fb3f0
SamuelCallWind <Samcallwind@gmail.com>
2023-06-23 09:46:35 +0200

    Added the fixed big box when clicking on any date, so that it follows. Added the dayLine

--- script.js
+++ script.js
@@ -2,37 +2,44 @@ window.addEventListener('DOMContentLoaded', (event) => {
     const currentDate = new Date();
     let daysInMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate();
     let dayBoxes = document.querySelectorAll('.boxDay');
-    let calendar = document.querySelector(".calendar");
+    let calendar = document.querySelector('.calendar');
     let currentMonth = new Date().toLocaleString('default', { month: 'long' });
     let newBox = document.createElement('div');
+    let newClose = document.createElement('div');
 
     function dayBoxGrow(e) {
-        newBox.classList = "dayBoxGrow";
-        newBox.style.display = "block";
+        newBox.classList.add('dayBoxGrow');
+        newBox.style.display = 'block';
         newBox.appendChild(createCloseButton());
         calendar.appendChild(newBox);
         let boxTitle = document.createElement('h1');
         boxTitle.innerHTML = `${currentMonth}  ${e.target.innerHTML}`;
         newBox.appendChild(boxTitle);
+        createDayLine(newBox);
         // Adding the two buttons to switch from day and night 
         newBox.appendChild(document.createElement('div')).classList.add('switchButtonOne');
         newBox.appendChild(document.createElement('div')).classList.add('switchButtonTwo');
-         
+        
     }
     function createCloseButton() {
-        let newClose = document.createElement('div');
-        newClose.addEventListener("click", () => {
+        newClose.addEventListener('click', () => {
             newBox.style.display = 'none';
             newBox.innerHTML = '';
         });   
-        newClose.classList = "closeButton";
+        newClose.classList = 'closeButton';
         newClose.innerHTML = '&times;'
         return newClose
+    }
+    
+    function createDayLine(box) {
+        let dayLine = document.createElement('div')
+        dayLine.classList = 'dayLine';
+        newBox.appendChild(dayLine);
     }
 
     for (i = 0; i < daysInMonth; i++) {
         dayBoxes[i].innerHTML = i + 1;
-        dayBoxes[i].addEventListener("click", (e) => dayBoxGrow(e));
+        dayBoxes[i].addEventListener('click', (e) => dayBoxGrow(e));
         
         
     }
